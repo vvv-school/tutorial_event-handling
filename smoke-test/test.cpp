70dfa2eba90be21545ce5edc532bdbbc84cbb1a9
Ugo Pattacini <ugo.pattacini@iit.it>
2017-10-15 20:00:10 +0200

    Update test.cpp

--- smoke-test/test.cpp
+++ smoke-test/test.cpp
@@ -45,15 +45,15 @@ public:
         //we need to load the data file into yarpdataplayer
         std::string cntlportname = "/playercontroller/rpc";
 
-        RTF_ASSERT_ERROR_IF(playercontroller.open(cntlportname),
-                            "Could not open RPC to yarpdataplayer");
+        RTF_ASSERT_ERROR_IF_FALSE(playercontroller.open(cntlportname),
+                                  "Could not open RPC to yarpdataplayer");
 
-        RTF_ASSERT_ERROR_IF(yarp::os::Network::connect(cntlportname, "/yarpdataplayer/rpc:i"),
-                            "Could not connect RPC to yarpdataplayer");
+        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect(cntlportname, "/yarpdataplayer/rpc:i"),
+                                  "Could not connect RPC to yarpdataplayer");
 
         //we need to check the output of yarpdataplayer is open and input of spiking model
-        RTF_ASSERT_ERROR_IF(yarp::os::Network::connect("/zynqGrabber/vBottle:o", "/event-handler/vBottle:i", "udp"),
-                            "Could not connect yarpdataplayer to spiking model");
+        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/zynqGrabber/vBottle:o", "/event-handler/vBottle:i", "udp"),
+                                  "Could not connect yarpdataplayer to spiking model");
 
         RTF_TEST_REPORT("Ports successfully open and connected");
 
@@ -75,14 +75,14 @@ public:
         yarp::os::Bottle cmd, reply;
         cmd.addString("play");
         playercontroller.write(cmd, reply);
-        RTF_ASSERT_ERROR_IF(reply.get(0).asString() == "ok", "Did not successfully play the dataset");
+        RTF_ASSERT_ERROR_IF_FALSE(reply.get(0).asString() == "ok", "Did not successfully play the dataset");
 
         yarp::os::Time::delay(5);
 
         cmd.clear();
         cmd.addString("stop");
         playercontroller.write(cmd, reply);
-        RTF_ASSERT_ERROR_IF(reply.get(0).asString() == "ok", "Did not successfully stop the dataset");
+        RTF_ASSERT_ERROR_IF_FALSE(reply.get(0).asString() == "ok", "Did not successfully stop the dataset");
 
     }
 };
